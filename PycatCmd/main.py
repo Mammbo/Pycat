9f289218c947346c98c3ae7c11556dc9d23ad388
Mammbo <113815614+Mammbo@users.noreply.github.com>
2024-12-06 11:56:28 -0600

    cmd init

---
+++ PycatCmd/main.py
@@ -0,0 +1,89 @@
+#imports 
+import os
+import subprocess
+import time
+import sys 
+import platform
+
+#Location of the wifi h2000 file
+wificap= "Path\\To\\Wifi\\Capture"
+# Directory where all the wordlist are located
+wordlistdir = "Pycat/PycatCmd/Wordlists"
+#Directory whee hashcat is located. Only for Windows users
+hashcatdir = "Path\\To\\Password\\Hashcat"
+#Directory where rules file ae located
+rulesdir = "Pycat/PycatCmd/Rules"
+#If for hashcat.hctune is located in a different directory than hashcat. This is needed if you get an error about hashtune is not found.
+hashtunedir = ""
+# Boolean value
+displayresults = True
+# Hashcat Mode
+mode = 22000
+
+
+# Gettting the OS platform to make sure if we need to run exe in Windows.
+plat = platform.system()
+
+if plat == "Windows":
+    hcat = "hashcat.exe"
+else:
+    hcat = "hashcat"
+
+options = ["\nAvailable Options:", "1. Attack", "2. View Available Wordlists/Rules/Charsets", "3. Upload Files", "4. Hash Identification", "5. Wordlist Munging", "6. Custom Rules", "7. View Potfile", "8. Exit"]
+
+# Pycat ASCII Art along with the several options needed 
+print(
+    """   _____                _____                    _____                    _____                _____                  
+         /\    \              |\    \                  /\    \                  /\    \              /\    \                 
+        /::\    \             |:\____\                /::\    \                /::\    \            /::\    \                
+       /::::\    \            |::|   |               /::::\    \              /::::\    \           \:::\    \               
+      /::::::\    \           |::|   |              /::::::\    \            /::::::\    \           \:::\    \              
+     /:::/\:::\    \          |::|   |             /:::/\:::\    \          /:::/\:::\    \           \:::\    \             
+    /:::/__\:::\    \         |::|   |            /:::/  \:::\    \        /:::/__\:::\    \           \:::\    \            
+   /::::\   \:::\    \        |::|   |           /:::/    \:::\    \      /::::\   \:::\    \          /::::\    \           
+  /::::::\   \:::\    \       |::|___|______    /:::/    / \:::\    \    /::::::\   \:::\    \        /::::::\    \          
+ /:::/\:::\   \:::\____\      /::::::::\    \  /:::/    /   \:::\    \  /:::/\:::\   \:::\    \      /:::/\:::\    \         
+/:::/  \:::\   \:::|    |    /::::::::::\____\/:::/____/     \:::\____\/:::/  \:::\   \:::\____\    /:::/  \:::\____\        
+\::/    \:::\  /:::|____|   /:::/~~~~/~~      \:::\    \      \::/    /\::/    \:::\  /:::/    /   /:::/    \::/    /        
+ \/_____/\:::\/:::/    /   /:::/    /          \:::\    \      \/____/  \/____/ \:::\/:::/    /   /:::/    / \/____/         
+          \::::::/    /   /:::/    /            \:::\    \                       \::::::/    /   /:::/    /                  
+           \::::/    /   /:::/    /              \:::\    \                       \::::/    /   /:::/    /                   
+            \::/____/    \::/    /                \:::\    \                      /:::/    /    \::/    /                    
+             ~~           \/____/                  \:::\    \                    /:::/    /      \/____/                     
+                                                    \:::\    \                  /:::/    /                                   
+                                                     \:::\____\                /:::/    /                                    
+                                                      \::/    /                \::/    /                                     
+                                                       \/____/                  \/____/                                      
+                                                                                                                             """) 
+while True: 
+    # prints all the options 
+    for i in options:
+        print("{}".format(i))
+    option = input("\nChoice: ")
+    match option:
+        case "1":
+            #hash cat attack and view the attack 
+            pass
+        case "2":
+            # print everything in charsets, rulles, and wordlists
+            pass
+        case "3":
+            # upload various files to the charsets, rules, or wordlists directory so users can make their own. 
+            pass
+        case "4":
+            # copy and paste a hash and have the program identify what type of hash it is 
+            pass
+        case "5":
+            # use crunch and cewl to munge wordlists together 
+            pass
+        case "6":
+            # Create Custom Rulesets 
+            pass
+        case "7":
+            # view cracked passwords 
+            pass
+        case "8":
+            # exit 
+            sys.exit(0)
+        case _:
+            pass
--- README.md
+++ README.md
@@ -25,6 +25,11 @@ make a gui and commandline version
 - [] : Button to identify hash.
 - [] Display hash type result.
 
+
+
+
+
+
 *Hashcat Attack* 
 [] run_hashcat_attack(): Execute Hashcat with selected options from user selections.
 [] display_output(): Append Hashcat output to GUI terminal.
@@ -51,3 +56,27 @@ Hash Identification
 [] pacakge binary to be run on every system
 [] make a requirements.txt with python dependencies 
 [] make a command-line version
+[] make window either automatically resize or disable it 
+
+
+------------------------------------------------------------
+
+*CMD Program* 
+*File Uploading*
+[] file uploading(): opens up box for you to add files to the specific folders. 
+File Handling Functions
+[] select_hash_file(): Open file dialog to select a hash file.
+[] select_wordlist(): Open file dialog for wordlist.
+[] select_rules_file(): Open file dialog for rules.
+
+Wordlist munging
+[] run_crunch(min_len, max_len, charset): Generate custom wordlists with Crunch.
+[] run_cewl(url): Scrape a website to generate a wordlist.
+
+Hash Identification
+[] identify_hash(hash_input): Use HashID to detect the hash type and display the result.
+
+*Hashcat Attack* 
+[] run_hashcat_attack(): Execute Hashcat with selected options from user selections.
+[] add hashcat functionality like when you are running hashcat
+(more buttons basically, and when you clicke them subprocess will execute something to do what you said)
